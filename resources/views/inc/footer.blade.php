2f2b3af7b415303ff110f699aa38c9fcb08be479
SashaMelva <sa1524517@gmail.com>
2023-05-31 17:58:55 +0300

    agent panel end

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -6,22 +6,23 @@
                     <img class="bi me-2" src="{{ Vite::asset('resources/img/logo-mini/3.png') }}" alt="logo">
                 </a>
             </div>
-            <p class="text-body-secondary">Контакты блабла</p>
+            <p class="text-body-secondary">Лучший сервисдля бронирования отелей</p>
         </div>
         <div class="col-6 col-md-2 mb-3">
             <ul class="nav flex-column">
+                <li class="nav-item mb-2"><a href="/" class="nav-link p-0 text-body-secondary">Главная</a></li>
                 <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Личный кабинет</a></li>
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Поулярные вопросы</a>
-                </li>
+{{--                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Поулярные вопросы</a>--}}
+{{--                </li>--}}
             </ul>
         </div>
         <div class="col-6 col-md-2 mb-3">
             <h5>Клиентам</h5>
             <ul class="nav flex-column">
                 <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Список отелей</a></li>
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Востановление
-                        аккаунта</a></li>
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Отзывы гостей</a></li>
+{{--                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Востановление--}}
+{{--                        аккаунта</a></li>--}}
+{{--                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Отзывы гостей</a></li>--}}
             </ul>
         </div>
 
@@ -34,7 +35,7 @@
     </div>
     <div class="d-flex flex-column flex-sm-row justify-content-between py-3 border-top">
         <div class="container block-center">
-            <a href="#" class="nav-link p-0 text-body-secondary">Политика хранения и обаботки персональных данных</a>
+            <a href="#" class="nav-link p-0 text-body-secondary"></a>
         </div>
     </div>
 
